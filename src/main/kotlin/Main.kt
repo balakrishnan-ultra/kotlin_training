5072cce3b4c464feb904d10c0eea6e9da636fa10
balakrishnan-ultra <balakrishnanultra01@gmail.com>
2025-11-10 21:07:46 +0530

    condition check : output not understand -> kotlin.Unit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,28 @@
 fun main()
 {
-    val heightAlice = 160
+    var heightAlice = 160
     val heightBob = 175
 
-    val taller = if (heightAlice > heightBob)
+    val taller = if (heightAlice < heightBob)
     {
         print("Choose Alice\n")
-        heightAlice
+        heightAlice=10
     } else {
         print("Choose Bob\n")
         heightBob
     }
 
     println("Taller height is $taller")
+    println("value changed heightAlice : $heightAlice")
 
 }
+
+
+/*
+How to work :
+
+Choose Alice
+Taller height is kotlin.Unit
+value changed heightAlice : 10
+
+ */
